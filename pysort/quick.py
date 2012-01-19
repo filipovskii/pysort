9e4962eea245f7c640afde9aa4b474e5b326f52d
filipovskii_off <filipovskii.off@gmail.com>
2012-01-19 23:38:49 +0400

    quicksort

--- pysort/quick.py
+++ pysort/quick.py
@@ -2,7 +2,6 @@ from random import randint
 
 def partition(a, frm, to):
     pivot = randint(frm, to)
-    new_pivot = pivot
     last_small = frm - 1
     last_big = frm
     for i in range(frm, to + 1):
@@ -12,10 +11,8 @@ def partition(a, frm, to):
             last_small += 1
             a[last_small], a[i] = a[i], a[last_small]
             if i == pivot:
-                new_pivot = last_small
-    a[new_pivot], a[last_small] = a[last_small], a[new_pivot]
-    print("a paritioned: {0}, pivot: a[{1}] = {2}"
-            .format(a[frm: to + 1], last_small, a[last_small]))
+                pivot = last_small
+    a[pivot], a[last_small] = a[last_small], a[pivot]
     return last_small # new pivot position
 
 def sort(a, frm = 0, to = None):
@@ -24,6 +21,8 @@ def sort(a, frm = 0, to = None):
     if to - frm < 1:
         return a
     pivot = partition(a, frm, to)
+    print("a into {0} + {1} + {2}"
+            .format(a[frm:pivot], a[pivot], a[pivot + 1: to + 1]))
     sort(a, frm, pivot - 1)
     sort(a, pivot + 1, to)
     return a
